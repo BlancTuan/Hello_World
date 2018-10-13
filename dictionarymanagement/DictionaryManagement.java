5fbbc96b74a1b54ee79ee7717295398683ba24e9
BlancTuan <43158523+BlancTuan@users.noreply.github.com>
2018-10-13 11:35:58 +0700

    Update dictionarymanagement/DictionaryManagement.java

--- dictionarymanagement/DictionaryManagement.java
+++ dictionarymanagement/DictionaryManagement.java
@@ -1,24 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dictionarymanagement;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileReader;
 import java.io.UnsupportedEncodingException;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 import java.io.Serializable;
-//import java.nio.charset.StandardCharsets;
+import java.io.Writer;
+import static java.nio.file.Files.delete;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,6 +29,7 @@ import java.util.logging.Logger;
 public class DictionaryManagement {
 
     public Dictionary dic = new Dictionary();
+    ArrayList<Word> arr = dic.arr;
 
     public void insertFromCommandline() {
 
@@ -41,91 +42,137 @@ public class DictionaryManagement {
             eng = input.nextLine();
             viet = input.nextLine();
             Word tuvung = new Word(eng, viet);
-            dic.arr.add(tuvung);
+            arr.add(tuvung);
 
         }
 
     }
-    
-    public void insertFromFile(){
 
-        try {   
+    public ArrayList<Word> insertFromFile() {
 
-               Scanner sc;
-            sc = new Scanner(new File("dictionari.txt"));
+        try {
+
+            Scanner sc;
+            File file = new File("dictionary.txt");
+            sc = new Scanner(file);
+           // System.out.println(file.getAbsolutePath());
             while (sc.hasNext()) {
+
                 String line = sc.nextLine();
                 String[] w;
-                w = line.split(":");
-                
+                w = line.split("\t");
+
                 Word tu = new Word(w[0], w[1]);
-              dic.arr.add(tu);
-            }               
+                arr.add(tu);
+
+            }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(DictionaryManagement.class.getName()).log(Level.SEVERE, null, ex);
         }
-                        
-        /*BufferedReader bf = null;
-        try {
-            String s;
-            bf = new BufferedReader(new InputStreamReader(new FileInputStream("dictionaries.txt")));
-            while ((s = bf.readLine()) != null) {
-                //s = bf.readLine();
-                String[] w = s.split(":");
-                Word tu = new Word(w[0], w[1]);
-                dic.arr.add(tu);
 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                bf.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
+        return arr;
     }
 
     public String dictionaryLookup(String s) {
-       
-        
-        //Collections.sort(dic.arr, new EngComparator());
-        Collections.sort(dic.arr, new Comparator<Word>()
-    {
-        @Override
-        public int compare(Word w1, Word w2)
-        {
-            return (w1.word_target.compareTo(w2.word_target));
+        String a = "";
+
+        try {
+
+            Scanner sc;
+            sc = new Scanner(new File("dictionary.txt"));
+
+            while (sc.hasNext()) {
+
+                String line = sc.nextLine();
+                String[] w;
+                w = line.split("\t");
+
+                //   Word tu = new Word(w[0], w[1]);
+                if (s.toLowerCase().trim().equals(w[0].toLowerCase().trim())) {
+                    a = w[1];
+                    break;
+                }
+
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DictionaryManagement.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-    );
-            String a = null;
-        int size = dic.arr.size(), i = 0, j = size - 1;
-          for(Word w : dic.arr)
-                
-             if(s.compareTo(w.word_target)==0)
-               a = (w.word_explain);
-           
-    
-         
-         
-        /*while (i < j) {
-            int mid = (i+j)/ 2;
-             int index = s.compareTo(dic.arr.get(mid).word_target);
-          
-                if(index == 0)
-                    a = dic.arr.get(mid).word_explain;
-                    
-                else if(index < 0)
-                    j = mid-1;
-                else 
-                    i = mid+1;
-            }*/
-     
-	
+//        for (Word w : arr) {
+//            if (s.toLowerCase().trim().equals(w.word_target)) {
+//                a = w.word_explain;
+//            }
+//        }
         return a;
     }
-}
-       
+
+    public void them() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap tu va nghia can them: ");
+        String target = sc.nextLine(), explain = sc.nextLine();
+        BufferedWriter bw = null;
+
+        try {
+
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dictionary.txt", true)));
+
+            bw.write(target + "\t" + explain + "\n");
+
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Da them!");
+    }
+
+    public void sua() {
+        this.insertFromFile();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nh?p tu va nghia can sua: ");
+        String tar = sc.nextLine();
+        String new_explain = sc.nextLine();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (tar.equals(arr.get(i).word_target)) {
+                arr.get(i).word_explain = new_explain;
+            }
+        }
+
+        System.out.println("Da sua!");
+
+    }
+
+    public void xoa() {
+        this.insertFromFile();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap tu Tieng Anh muon xoa: ");
+        String tar = sc.nextLine();
+        //insertFromFile();
+        for (int i = 0; i < arr.size(); i++) {
+            if (tar.equals(arr.get(i).word_target)) {
+                arr.remove(i);
+            }
+        }
+        System.out.println("Da xoa!");
+
+    }
+
+    public void dictionaryExportToFile() throws IOException {
+        BufferedWriter bw = null;
         
+        try {
+
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\MyPC\\OneDrive\\Documents\\NetBeansProjects\\DictionaryManagement\\src\\dictionarymanagement\\dictionary.txt")));
+
+            for (int i = 0; i < arr.size(); i++) {
+
+                bw.write(arr.get(i).word_target + "\t" + arr.get(i).word_explain + "\r\n");
+            }
+
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
